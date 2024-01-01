f32b6b77eabad56922d8650a31a9fcbf1e57f52f
Chris Klanac <1008777+cklanac@users.noreply.github.com>
2024-01-01 16:39:53 -0500

    fix walklthrough output url

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -21,7 +21,7 @@ output "frontend_url" {
 
 output "neos_toc_url" {
   description = "Tutorial URL"
-  value       = "http://console.cloud.google.com/products/solutions/deployments?walkthrough_id=panels--sic--dynamic-python-web-app_toc"
+  value       = "http://console.cloud.google.com/products/solutions/deployments?walkthrough_id=panels--sic--dynamic-javascript-web-app_toc"
 }
 
 output "run_service_name" {
